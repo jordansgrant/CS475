9c7d2c488082832a3953f37c7fbb4b1f117ea321
jordansgrant <jordans.grant@gmail.com>
2017-05-28 18:37:37 -0700

    prog6 part1 working

---
+++ prog6/runprog.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+
+my @rowsMult;
+my @rowsMultSum;
+my @locals = ( 8, 32, 64, 128, 256, 512 );
+
+foreach my $global ( 16, 160, 1280, 10240, 64000, 20000, 80000, 125000 ) {
+  $globalSize = $global * 64;
+
+  my $lineMult    = "".$globalSize;
+  my $lineMultSum = "".$globalSize;
+
+  foreach my $local ( @locals ) {
+    $retval = system("g++ -o prog prog6.cpp -lm -fopenmp -lOpenCL -DNUM=".$global." -DLOCAL_SIZE=".$local." -DMULT");
+    if ( $retval != 0 ) {
+      print STDERR "Could not compile with Global=64*".$global." Local Size=".$local." with Mult f(x)\n";
+      return 1;
+    }  
+    $retval = `prog`;
+    $lineMult = $lineMult."\t".$retval;
+  
+    $retval = system("g++ -o prog prog6.cpp -lm -fopenmp -lOpenCL -DNUM=".$global." -DLOCAL_SIZE=".$local." -DMULTSUM");
+    if ( $retval != 0 ) {
+      print STDERR "Could not compile with Global=64*".$global." Local Size=".$local." with MultSum f(x)\n";
+      return 1;
+    }  
+    $retval = `prog`;
+    $lineMultSum = $lineMultSum."\t".$retval;
+  }
+  
+  push @rowsMult,    $lineMult."\n";
+  push @rowsMultSum, $lineMultSum."\n";
+}
+
+print "\nMult Table\n";
+foreach my $local (@locals) {
+  print "\t".$local;
+}
+print "\n";
+
+foreach my $row (@rowsMult) {
+  print $row;
+}
+
+print "\nMultSum Table\n";
+foreach my $local (@locals) {
+  print "\t".$local;
+}
+print "\n";
+
+foreach my $row (@rowsMultSum) {
+  print $row;
+}
+
+system("rm -f prog");
